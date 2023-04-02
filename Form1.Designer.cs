aceb4d909bee1682e6de62600af742847b6c401a
Mirko <MirkoBermudezAyvar@gmail.com>
2023-04-02 14:58:22 -0500

    Priemra entrega

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Prueba_pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            hola1ToolStripMenuItem = new ToolStripMenuItem();
+            hi1ToolStripMenuItem = new ToolStripMenuItem();
+            h2ToolStripMenuItem = new ToolStripMenuItem();
+            hola2ToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1 = new MenuStrip();
+            hola1ToolStripMenuItem1 = new ToolStripMenuItem();
+            hola2ToolStripMenuItem1 = new ToolStripMenuItem();
+            hi2ToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            documentosToolStripMenuItem = new ToolStripMenuItem();
+            ventanasToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            toolStripProgressBar1 = new ToolStripProgressBar();
+            toolStripContainer1 = new ToolStripContainer();
+            printDialog1 = new PrintDialog();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            openFileDialog1 = new OpenFileDialog();
+            imageList1 = new ImageList(components);
+            process1 = new System.Diagnostics.Process();
+            openFileDialog2 = new OpenFileDialog();
+            bindingSource1 = new BindingSource(components);
+            treeView1 = new TreeView();
+            listView1 = new ListView();
+            mantenimientoDePlantillasToolStripMenuItem = new ToolStripMenuItem();
+            mantenimientoDeUsuariosToolStripMenuItem = new ToolStripMenuItem();
+            cerrarSesiónToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { hola1ToolStripMenuItem, hola2ToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(106, 48);
+            contextMenuStrip1.UseWaitCursor = true;
+            // 
+            // hola1ToolStripMenuItem
+            // 
+            hola1ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { hi1ToolStripMenuItem, h2ToolStripMenuItem });
+            hola1ToolStripMenuItem.Name = "hola1ToolStripMenuItem";
+            hola1ToolStripMenuItem.Size = new Size(105, 22);
+            hola1ToolStripMenuItem.Text = "Hola1";
+            // 
+            // hi1ToolStripMenuItem
+            // 
+            hi1ToolStripMenuItem.Name = "hi1ToolStripMenuItem";
+            hi1ToolStripMenuItem.Size = new Size(90, 22);
+            hi1ToolStripMenuItem.Text = "hi1";
+            // 
+            // h2ToolStripMenuItem
+            // 
+            h2ToolStripMenuItem.Name = "h2ToolStripMenuItem";
+            h2ToolStripMenuItem.Size = new Size(90, 22);
+            h2ToolStripMenuItem.Text = "h2";
+            // 
+            // hola2ToolStripMenuItem
+            // 
+            hola2ToolStripMenuItem.Name = "hola2ToolStripMenuItem";
+            hola2ToolStripMenuItem.Size = new Size(105, 22);
+            hola2ToolStripMenuItem.Text = "Hola2";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { hola1ToolStripMenuItem1, hola2ToolStripMenuItem1, toolStripMenuItem1, documentosToolStripMenuItem, ventanasToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1600, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // hola1ToolStripMenuItem1
+            // 
+            hola1ToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { mantenimientoDePlantillasToolStripMenuItem, mantenimientoDeUsuariosToolStripMenuItem, cerrarSesiónToolStripMenuItem, salirToolStripMenuItem });
+            hola1ToolStripMenuItem1.Name = "hola1ToolStripMenuItem1";
+            hola1ToolStripMenuItem1.Size = new Size(60, 20);
+            hola1ToolStripMenuItem1.Text = "Archivo";
+            // 
+            // hola2ToolStripMenuItem1
+            // 
+            hola2ToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { hi2ToolStripMenuItem });
+            hola2ToolStripMenuItem1.Name = "hola2ToolStripMenuItem1";
+            hola2ToolStripMenuItem1.Size = new Size(35, 20);
+            hola2ToolStripMenuItem1.Text = "Ver";
+            // 
+            // hi2ToolStripMenuItem
+            // 
+            hi2ToolStripMenuItem.Name = "hi2ToolStripMenuItem";
+            hi2ToolStripMenuItem.Size = new Size(90, 22);
+            hi2ToolStripMenuItem.Text = "hi2";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(76, 20);
+            toolStripMenuItem1.Text = "Busquedas";
+            // 
+            // documentosToolStripMenuItem
+            // 
+            documentosToolStripMenuItem.Name = "documentosToolStripMenuItem";
+            documentosToolStripMenuItem.Size = new Size(87, 20);
+            documentosToolStripMenuItem.Text = "Documentos";
+            // 
+            // ventanasToolStripMenuItem
+            // 
+            ventanasToolStripMenuItem.Name = "ventanasToolStripMenuItem";
+            ventanasToolStripMenuItem.Size = new Size(66, 20);
+            ventanasToolStripMenuItem.Text = "Ventanas";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripProgressBar1 });
+            statusStrip1.Location = new Point(0, 570);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1600, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            toolStripProgressBar1.Name = "toolStripProgressBar1";
+            toolStripProgressBar1.Size = new Size(100, 16);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            toolStripContainer1.ContentPanel.Size = new Size(150, 150);
+            toolStripContainer1.Location = new Point(0, 85);
+            toolStripContainer1.Name = "toolStripContainer1";
+            toolStripContainer1.Size = new Size(150, 175);
+            toolStripContainer1.TabIndex = 3;
+            toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // printDialog1
+            // 
+            printDialog1.UseEXDialog = true;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // process1
+            // 
+            process1.StartInfo.Domain = "";
+            process1.StartInfo.LoadUserProfile = false;
+            process1.StartInfo.Password = null;
+            process1.StartInfo.StandardErrorEncoding = null;
+            process1.StartInfo.StandardInputEncoding = null;
+            process1.StartInfo.StandardOutputEncoding = null;
+            process1.StartInfo.UserName = "";
+            process1.SynchronizingObject = this;
+            // 
+            // openFileDialog2
+            // 
+            openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(153, 85);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(319, 372);
+            treeView1.TabIndex = 8;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(478, 85);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(989, 294);
+            listView1.TabIndex = 9;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.ItemActivate += listView1_ItemActivate_1;
+            // 
+            // mantenimientoDePlantillasToolStripMenuItem
+            // 
+            mantenimientoDePlantillasToolStripMenuItem.Name = "mantenimientoDePlantillasToolStripMenuItem";
+            mantenimientoDePlantillasToolStripMenuItem.Size = new Size(222, 22);
+            mantenimientoDePlantillasToolStripMenuItem.Text = "Mantenimiento de Plantillas";
+            // 
+            // mantenimientoDeUsuariosToolStripMenuItem
+            // 
+            mantenimientoDeUsuariosToolStripMenuItem.Name = "mantenimientoDeUsuariosToolStripMenuItem";
+            mantenimientoDeUsuariosToolStripMenuItem.Size = new Size(222, 22);
+            mantenimientoDeUsuariosToolStripMenuItem.Text = "Mantenimiento de Usuarios";
+            // 
+            // cerrarSesiónToolStripMenuItem
+            // 
+            cerrarSesiónToolStripMenuItem.Name = "cerrarSesiónToolStripMenuItem";
+            cerrarSesiónToolStripMenuItem.Size = new Size(222, 22);
+            cerrarSesiónToolStripMenuItem.Text = "Cerrar Sesión";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(222, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1600, 592);
+            Controls.Add(listView1);
+            Controls.Add(treeView1);
+            Controls.Add(toolStripContainer1);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            contextMenuStrip1.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            toolStripContainer1.ResumeLayout(false);
+            toolStripContainer1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem hola1ToolStripMenuItem;
+        private ToolStripMenuItem hi1ToolStripMenuItem;
+        private ToolStripMenuItem h2ToolStripMenuItem;
+        private ToolStripMenuItem hola2ToolStripMenuItem;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem hola1ToolStripMenuItem1;
+        private ToolStripMenuItem hola2ToolStripMenuItem1;
+        private ToolStripMenuItem hi2ToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private StatusStrip statusStrip1;
+        private ToolStripProgressBar toolStripProgressBar1;
+        private ToolStripContainer toolStripContainer1;
+        private PrintDialog printDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private OpenFileDialog openFileDialog1;
+        private ImageList imageList1;
+        private System.Diagnostics.Process process1;
+        private OpenFileDialog openFileDialog2;
+        private BindingSource bindingSource1;
+        private TreeView treeView1;
+        private ToolStripMenuItem documentosToolStripMenuItem;
+        private ToolStripMenuItem ventanasToolStripMenuItem;
+        private ListView listView1;
+        private ToolStripMenuItem mantenimientoDePlantillasToolStripMenuItem;
+        private ToolStripMenuItem mantenimientoDeUsuariosToolStripMenuItem;
+        private ToolStripMenuItem cerrarSesiónToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+    }
+
+}
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+using System.Diagnostics;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Prueba_pdf
+{
+    public partial class Form1 : Form
+    {
+        private string currentDirectory;
+        private void MostrarArchivos(string directorio)
+        {
+            listView1.Items.Clear();
+
+            currentDirectory = directorio;
+
+            try
+            {
+                string[] dirs = Directory.GetDirectories(directorio);
+                foreach (string dir in dirs)
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+                    ListViewItem item = new ListViewItem(di.Name, 0);
+                    item.SubItems.Add("Folder");
+                    item.SubItems.Add(di.LastAccessTime.ToString());
+                    listView1.Items.Add(item);
+                }
+
+                string[] files = Directory.GetFiles(directorio);
+                foreach (string file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    ListViewItem item = new ListViewItem(fi.Name, 1);
+                    item.SubItems.Add(fi.Extension);
+                    item.SubItems.Add(fi.LastAccessTime.ToString());
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            CrearCarpetas();
+
+            string path2 = Path.Combine(Application.StartupPath, "imagenes\\carpeta.png");
+            ImageList imageList = new ImageList();
+            imageList.Images.Add("Folder", Image.FromFile("D:\\PROYECTOS_VS\\Prueba_pdf\\Prueba_pdf\\imagenes\\carpeta.png")); // El nombre "Folder" se usa como clave para recuperar esta imagen en el futuro
+            treeView1.ImageList = imageList;
+            string path = @"D:\CarpetasPDF"; // Ruta del directorio que desea mostrar
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            TreeNode rootNode = new TreeNode(dirInfo.Name);
+            rootNode.Tag = dirInfo;
+            GetDirectories(dirInfo.GetDirectories(), rootNode);
+            treeView1.Nodes.Add(rootNode);
+
+            // Configura el ListView en modo de detalle
+            listView1.View = View.Details;
+
+            // Agrega las columnas al ListView
+            listView1.Columns.Add("Nombre", 200);
+            listView1.Columns.Add("Fecha de modificación", 120);
+            listView1.Columns.Add("Tipo", 80);
+            listView1.Columns.Add("Tamaño", 80);
+        }
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "Folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string ruta = e.Node.FullPath;
+            if (ruta.StartsWith(@"\")) ruta = ruta.Substring(1);
+            ruta = @"D:\" + ruta;
+
+            listView1.Clear();
+            listView1.Columns.Add("Nombre", 200); // Columna para el nombre de la subcarpeta
+            listView1.Columns.Add("Fecha de creación", 150); // Columna para la fecha de creación de la subcarpeta
+            MostrarArchivos(e.Node.FullPath);
+            try
+            {
+                string[] subDirectorios = Directory.GetDirectories(ruta);
+                foreach (string subDirectorio in subDirectorios)
+                {
+                    string nombreDirectorio = Path.GetFileName(subDirectorio);
+                    string fechaCreacion = Directory.GetCreationTime(subDirectorio).ToString();
+                    ListViewItem item = new ListViewItem(new string[] { nombreDirectorio, fechaCreacion });
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void listView1_ItemActivate_1(object sender, EventArgs e)
+
+
+        {
+            ListViewItem selectedItem = listView1.SelectedItems[0];
+
+            if (selectedItem.SubItems[1].Text == "Folder")
+            {
+                string newDirectory = Path.Combine(currentDirectory, selectedItem.Text);
+                MostrarArchivos(newDirectory);
+            }
+            else
+            {
+                string fileName = selectedItem.Text;
+                string filePath = Path.Combine(currentDirectory, fileName);
+                MessageBox.Show($"Abriendo archivo {filePath}");
+            }
+        }
+
+        private void CrearCarpetas()
+        {
+            // Generar un número aleatorio de subcarpetas
+            Random random = new Random();
+            int numSubcarpetas = random.Next(1, 11);
+
+            // Crear la carpeta principal
+            string carpetaPrincipal = @"D:\CarpetasPDF";
+            if (!Directory.Exists(carpetaPrincipal))
+            {
+                Directory.CreateDirectory(carpetaPrincipal);
+            }
+
+            // Crear las subcarpetas con nombres generados automáticamente
+            for (int i = 1; i <= numSubcarpetas; i++)
+            {
+                string nombreSubcarpeta = "F00" + i.ToString();
+                string rutaSubcarpeta = Path.Combine(carpetaPrincipal, nombreSubcarpeta);
+                Directory.CreateDirectory(rutaSubcarpeta);
+
+            }
+        }
+    }
+
+
+
+}
+
+
+
